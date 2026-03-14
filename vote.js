b463e9140e0809c45a81b7e008c5ba288d1dd3f3
dhanu67hp <puttagangadharappahs@gmail.com>
2026-03-14 11:03:55 +0530

    hi

---
+++ vote.js
@@ -0,0 +1,10 @@
+function vote(){
+   
+    var age = document.getElementById("age").value;
+    var res = document.getElementById("res").value;
+}
+    if(age>=18){
+        res.innerHTML =name+""+"Eligible to vote"
+    }else{
+        res.innerHTML =name+""+"you are still child"
+    }
